65fff0940ea66510aa45ed1b9fb2dca7ea30ad06
Jenidevops <jenifernirmalraj@gmail.com>
2025-09-21 04:23:01 -0700

    updated files

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,9 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/Task-Manager/',
   plugins: [
-  tailwindcss(),
-  react()
-
+    tailwindcss(),
+    react()
   ],
 })
